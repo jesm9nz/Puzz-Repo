058ddfd46d506d8771eb1f2618a6dc856c494292
jesm9nz <45139985+jesm9nz@users.noreply.github.com>
2020-08-29 22:48:26 +1200

    Update index.php
    Basic HTML structure for now

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-<p>if this is what it feels to love, then I can feel it</p>
+<!-- PUZZ v0.0.1 !-->
+<html>
+<head>
+	<title>Puzz Storywriter v0.0.1</title>
+</head>
+<body>
+	<h1>Puzz Storywriter v0.0.1</h1>
+	<p>if this is what it feels to love, then I can feel it</p>
+	<p>&copy; 2020 Puzz Storywriter v0.0.1</p>
+</body>
+</html>
