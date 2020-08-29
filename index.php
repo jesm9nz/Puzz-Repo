71f676bd012153c91889b65cef39ecbeb1afa793
jesm9nz <45139985+jesm9nz@users.noreply.github.com>
2020-08-29 23:09:32 +1200

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,14 @@
 <html>
 <head>
 	<title>Puzz Storywriter v0.0.1</title>
+	<link rel="stylesheet" type="text/css" href="style/css_default.css">
 </head>
 <body>
 	<h1>Puzz Storywriter v0.0.1</h1>
-	<p>if this is what it feels to love, then I can feel it</p>
-	<p>&copy; 2020 Puzz Storywriter v0.0.1</p>
+	
+	<div id ="footer">
+		<p>&copy; 2020 Puzz Storywriter v0.0.1</p>
+		<p>if this is what it feels to love, then I can feel it</p>
+	</div>
 </body>
 </html>
